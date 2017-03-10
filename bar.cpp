1ed0640745fe919eee058f6c826633af90b41c10
eldrom <eldrom@users.noreply.github.com>
2017-03-10 11:40:42 +0300

    Update bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -1,3 +1,10 @@
 int Invert( int number ) {
     return -1*number;
 }
+
+TEST_CASE( "Invert", "[invert]" ) {
+    REQUIRE( Invert(1) == -1 );
+    REQUIRE( Invert(2) == -2 );
+    REQUIRE( Invert(0) == 0 );
+    REQUIRE( Invert(-10) == 10 );
+}
