237f7a02fcef3ad050a91ea4958bf73cec0173e3
eldrom <eldrom@users.noreply.github.com>
2017-03-10 11:43:03 +0300

    Update foo.cpp

--- foo.cpp
+++ foo.cpp
@@ -1,3 +1,10 @@
 unsigned int Mod(int number ) {
     return number < 0 ? -1*number : number;
 }
+
+TEST_CASE( "Mod", "[mod]" ) {
+    REQUIRE( Mod(1) == 1 );
+    REQUIRE( Mod(-2) == 2 );
+    REQUIRE( Mod(0) == 0 );
+    REQUIRE( Mod(10) == 10 );
+}
